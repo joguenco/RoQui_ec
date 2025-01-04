e52bd3dc3b861cb27364bcd8117ac73a51ee4b92
Jorge Luis <jorjoluiso@gmail.com>
2025-01-04 14:23:09 -0500

    [ADD] Initial code

---
+++ src/main/kotlin/dev/joguenco/roqui/RoQuiApplication.kt
@@ -0,0 +1,11 @@
+package dev.joguenco.roqui
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoQuiEcApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoQuiEcApplication>(*args)
+}
