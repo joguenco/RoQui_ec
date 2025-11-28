b29facaf4ec972b3b8b4446fd92c043d94680180
joguenco <jorgeluis@resolvedor.dev>
2025-11-28 15:13:20 -0500

    [FIX] Exclude login requests from adding Authorization header

--- roqui-client/src/services/client.js
+++ roqui-client/src/services/client.js
@@ -15,6 +15,24 @@ export function setAuthStore(store) {
 // Request interceptor: Add access token to requests
 client.interceptors.request.use(
   (config) => {
+    // Exclude login requests from adding Authorization header
+    const getRequestPath = (cfg) => {
+      const rawUrl = cfg.url || ''
+      const base = cfg.baseURL || client.defaults.baseURL || ''
+      try {
+        const u = new URL(rawUrl, base || 'http://localhost')
+        return u.pathname
+      } catch {
+        return rawUrl.split(/[?#]/)[0]
+      }
+    }
+
+    const requestPath = getRequestPath(config)
+    if (requestPath === '/login') {
+      return config
+    }
+    // Exclude login requests from adding Authorization header
+
     if (authStore && authStore.getAccessToken()) {
       config.headers.Authorization = `Bearer ${authStore.getAccessToken()}`
     }
@@ -44,6 +62,24 @@ client.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config
+    // Exclude login requests from adding Authorization header
+    const getRequestPath = (cfg) => {
+      const rawUrl = cfg?.url || ''
+      const base = cfg?.baseURL || client.defaults.baseURL || ''
+      try {
+        const u = new URL(rawUrl, base || 'http://localhost')
+        return u.pathname
+      } catch {
+        return rawUrl.split(/[?#]/)[0]
+      }
+    }
+    const requestPath = getRequestPath(originalRequest)
+
+    // Do not try to refresh tokens for login requests
+    if (requestPath === '/login') {
+      return Promise.reject(error)
+    }
+    // Exclude login requests from adding Authorization header
 
     // If error is 401 and we haven't tried to refresh yet
     if (error.response?.status === 401 && !originalRequest._retry) {
