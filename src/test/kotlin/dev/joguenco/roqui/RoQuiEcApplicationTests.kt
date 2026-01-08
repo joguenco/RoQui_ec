0ab54118fd94b98f51c876c5a2b8f02d969f9a6c
Jorge Luis <jorgeluis@resolvedor.dev>
2026-01-07 21:25:23 -0500

    [ADD] Test for ping API Rest

--- src/test/kotlin/dev/joguenco/roqui/RoQuiEcApplicationTests.kt
+++ src/test/kotlin/dev/joguenco/roqui/RoQuiEcApplicationTests.kt
@@ -1,10 +1,28 @@
 package dev.joguenco.roqui
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RoQuiEcApplicationTests {
 
+    @Autowired lateinit var mockMvc: MockMvc
+
     @Test fun contextLoads() {}
+
+    @Test
+    fun `GET ping should return pong`() {
+        mockMvc
+            .perform(get("/ping"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.message").value("pong"))
+    }
 }
